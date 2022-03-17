0121171ad66d1b9fb9ef391736d7a6a13fd669cd
sai <shankardevops214@gmail.com>
2022-03-17 20:07:52 +0530

    sum.java v1.0

---
+++ sum.java
@@ -0,0 +1,13 @@
+class sum
+{
+	public static void main (string [] args)
+	{
+		int a,b,c;
+		a=50;
+		b=60;
+		c=120;
+		a+b+c;
+              	system.out.println("the sum of values are"+c
+}
+//dckdjndjcncjisncjdssdjseojsjefslssjkjsls
+
